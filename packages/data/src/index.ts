cf358f8ccb8028815caf8907a2f225ef372d7435
Thanasis Gliatis <thanasis.glts@gmail.com>
2026-05-05 01:12:01 +0300

    feat(data): add FearAndGreedSource using Alternative.me API

--- packages/data/src/index.ts
+++ packages/data/src/index.ts
@@ -1,3 +1,4 @@
 export type { DataSource } from './sources/base.js'
 export { NullDataSource } from './sources/null.js'
+export { FearAndGreedSource } from './sources/fear-and-greed.js'
 export { Pipeline } from './pipeline.js'
